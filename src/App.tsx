b1ff39ff24d64a70016f60493bb8466541b9cd0e
Byron Yego <byronyego@gmail.com>
2022-07-30 16:56:32 +0300

    real layout complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="wrapper">
       <div className="left">navbar goes here</div>
-      <div className="right">main body goes here</div>
+      <div className="right">
+        <div className="topbar">topbar location here</div>
+        <div className="artist">artist profile goes here</div>
+        <div className="lower">
+          <div className="genre">
+            <div className="topartists">top artists section</div>
+            <div className="chartsalot">
+              <div className="genres">genres hapa deh</div>
+              <div className="topcharts">top charts</div>
+            </div>
+          </div>
+          <div className="player">player goes here</div>
+        </div>
+      </div>
     </div>
   );
 }
