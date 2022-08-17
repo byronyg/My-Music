4060266d4878ae176dc1c1585204a8dec2ad709b
Byron Yego <byronyego@gmail.com>
2022-08-17 13:39:51 +0300

    remove ts

---
+++ src/utils/StateProvider.jsx
@@ -0,0 +1,10 @@
+import { createContext, useContext, useReducer } from "react";
+export const StateContext = createContext();
+
+export const StateProvider = ({ children, initialState, reducer }: any) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateProvider = () => useContext(StateContext);
